cf0c61c5cc199f34bff3ce0e40218d7c24fd3dfd
nemanja xps <nemanjajurisic@gmail.com>
2022-01-31 13:34:33 +0100

    Revert "Added clients, openfeign, client exception handling, token negotiation with keycloak"
    This reverts commit bbd401ec99afcd790263291b2c4b3f15404c2ce5.

--- src/main/java/com/uraditi/backend/controller/UserController.java
+++ src/main/java/com/uraditi/backend/controller/UserController.java
@@ -1,55 +1,33 @@
 package com.uraditi.backend.controller;
 
-import com.uraditi.backend.client.KeycloakClient;
+import com.uraditi.backend.dto.UserDto;
 import com.uraditi.backend.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.RolesAllowed;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @RestController("User controller")
 @RequestMapping("/api/users")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-//@ApiResponses({
-//        @ApiResponse(responseCode = "500", description = errorMessageDto.class)
-//})
 public class UserController {
 
     private final UserService userService;
-    private final KeycloakClient keycloakClient;
 
     // http://localhost:8080/swagger-ui/index.html
 
-    @PostMapping("/create")
-    @RolesAllowed("uradiTi_admin")
-    public ResponseEntity createUser(@RequestParam @NotNull @NotEmpty String username,
-                                     @RequestParam @NotNull @NotEmpty String password) {
-        return ResponseEntity.ok(userService.createUser(username, password));
+    @PostMapping("/save")
+    @RolesAllowed("user")
+    public ResponseEntity<UserDto> saveUser(@RequestBody UserDto userDto) {
+        return ResponseEntity.ok(userService.save(userDto));
     }
 
     @GetMapping("/all")
-    @RolesAllowed("uradiTi_admin")
-    public ResponseEntity getAll() {
-        return ResponseEntity.ok(keycloakClient.getAllUsers());
+    @RolesAllowed({"uradi_ti_user", "uradi_ti_admin"})
+    public ResponseEntity<List<UserDto>> getAll() {
+        return ResponseEntity.ok(userService.getAllUsers());
     }
-
-    @GetMapping("/{username}")
-    @RolesAllowed("uradiTi_admin")
-    public ResponseEntity<List<UserRepresentation>> findUserByUsername(@PathVariable("username") String username) {
-        return ResponseEntity.ok(userService.findByUsername(username));
-    }
-
-    @DeleteMapping("/{id}")
-    @RolesAllowed("uradiTi_admin")
-    public Object deleteUserById(@PathVariable("id") String id) {
-        return ResponseEntity.ok(userService.delete(id));
-    }
-
-
 }
--- src/main/java/com/uraditi/backend/exception/AutoConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.uraditi.backend.exception;
-
-import org.springframework.context.annotation.ComponentScan;
-
-@ComponentScan
-public class AutoConfiguration {
-}
